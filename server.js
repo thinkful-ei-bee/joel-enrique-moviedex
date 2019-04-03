fe3a590526d6250773ccb4fdaad300ca621208b3
joelcorey <joelcorey@fastmail.com>
2019-04-03 08:47:20 -0700

    server listen

--- server.js
+++ server.js
@@ -42,3 +42,9 @@ app.get('/movie', function handleGetMovie(req, res) {
 
   res.json(response)
 })
+
+const PORT = 8000
+
+app.listen(PORT, () => {
+  console.log(`Server listening at http://localhost:${PORT}`)
+})
