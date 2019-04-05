bde80a078356aa7f782059332c8a8db157daa537
joelcorey <joelcorey@fastmail.com>
2019-04-05 09:15:46 -0700

    port to env variable

--- server.js
+++ server.js
@@ -61,7 +61,7 @@ app.get('/movie', function handleGetMovie(req, res) {
   res.json(response)
 })
 
-const PORT = 8000
+const PORT = process.env.PORT || 8000
 
 app.listen(PORT, () => {
   console.log(`Server listening at http://localhost:${PORT}`)
